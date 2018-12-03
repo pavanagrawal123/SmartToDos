540b3f2b7e2ef3d44b69ba75674cb6ac4a93f5f0
Pavan Agrawal <pavanagrawal@outlook.com>
2018-12-02 22:57:52 -0600

    updated ui

--- smartTodos/pages/index.vue
+++ smartTodos/pages/index.vue
@@ -1,18 +1,22 @@
 <template>
   <v-layout column justify-center align-center>
     <v-container grid-list-md align-center text-xs-center>
+      <h1> Your todo list will take {{ Math.round(this.time) }} minutes to complete </h1>
       <v-layout align-content-center row wrap>
         <v-flex xs3>
           <v-text-field v-model="todo" label="New Todo"></v-text-field>
         </v-flex>
-        <v-flex xs3>
+        <v-flex xs2>
           <v-btn @click="addTodo">Add</v-btn>
         </v-flex>
-        <v-flex xs3>
+        <v-flex xs2>
           <v-btn @click="Prioritize">Prioritize Tasks</v-btn>
         </v-flex>
         <v-flex xs3>
           <v-btn @click="Time">Predict Time Needed</v-btn>
+        </v-flex>
+        <v-flex xs2>
+          <v-btn @click="Dummy">Load Dummy Data</v-btn>
         </v-flex>
       </v-layout>
     </v-container>
@@ -29,7 +33,7 @@
           <td>{{ props.item.text }}</td>
           <td>{{ props.item.time }}</td>
           <td>
-            <v-btn :disabled="!valid" @click="removeTodo(props.item.id)">Completed</v-btn>
+            <v-btn  @click="removeTodo(props.item.id)">Completed</v-btn>
           </td>
         </template>
       </v-data-table>
@@ -45,7 +49,7 @@
           <td>{{ props.item.text }}</td>
           <td>{{ props.item.time }}</td>
           <td>
-            <v-btn :disabled="!valid" @click="removeTodo(props.item.id)">Completed</v-btn>
+            <v-btn  @click="removeTodo(props.item.id)">Completed</v-btn>
           </td>
         </template>
       </v-data-table>
@@ -61,7 +65,7 @@
           <td>{{ props.item.text }}</td>
           <td>{{ props.item.time }}</td>
           <td>
-            <v-btn :disabled="!valid" @click="removeTodo(props.item.id)">Completed</v-btn>
+            <v-btn @click="removeTodo(props.item.id)">Completed</v-btn>
           </td>
         </template>
       </v-data-table>
@@ -77,7 +81,7 @@
           <td>{{ props.item.text }}</td>
           <td>{{ props.item.time }}</td>
           <td>
-            <v-btn :disabled="!valid" @click="removeTodo(props.item.id)">Completed</v-btn>
+            <v-btn @click="removeTodo(props.item.id)">Completed</v-btn>
           </td>
         </template>
       </v-data-table>
@@ -99,7 +103,8 @@ export default {
       todo: '',
       headers: [],
       todoList: [],
-      count: -1
+      count: -1,
+      time: 0,
     }
   },
   methods: {
@@ -141,10 +146,32 @@ export default {
               'http://ec2-54-172-93-154.compute-1.amazonaws.com:5000/time?task=' + this.todoList[i].text
             )
             .then(response => {
-              this.todoList[i].time = response.data
+              this.todoList[i].time = response.data;
+              this.time+= response.data;
             })
         }
       }
+    },
+    Dummy: function() {
+      this.todoList =[
+        { id: 0, text: "Calculus Homework", priority: null, time: null},
+        { id: 1, text: "Physics Homework", priority: null, time: null},
+        { id: 2, text: "English Paper", priority: null, time: null},
+        { id: 3, text: "Eat Lunch", priority: null, time: null},
+        { id: 4, text: "Do Laundry", priority: null, time: null},
+        { id: 5, text: "Call insurance company to renew", priority: null, time: null},
+        { id: 6, text: "pay elctricity bill", priority: null, time: null},
+        { id: 7, text: "drive to school and pickup kid from school", priority: null, time: null},
+        { id: 8, text: "mow the lawn", priority: null, time: null},
+        { id: 9, text: "reserve table at restaurant", priority: null, time: null},
+        { id: 10, text: "purchase tickets", priority: null, time: null},
+        { id: 11, text: "work on the book report.", priority: null, time: null},
+        { id: 12, text: "Call the doctor's office to make an appointment", priority: null, time: null},
+        { id: 13, text: "go to store and buy present for birthday party", priority: null, time: null},
+
+      ]
+      this.size = 14;
+      this.time = 0;
     }
   }
 }
